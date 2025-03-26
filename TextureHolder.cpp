f24ff1ffc5cd156dbcdd089c30437c176f2c0d18
Brian Wheeler <brianwheeler810@gmail.com>
2025-03-25 19:43:52 -0700

    Add Zombie class

--- TextureHolder.cpp
+++ TextureHolder.cpp
@@ -1,6 +1,8 @@
 #include "TextureHolder.h"
 #include <assert.h>
 
+TextureHolder* TextureHolder::m_s_Instance = nullptr;
+
 TextureHolder::TextureHolder() {
 	assert(m_s_Instance == nullptr);
 	m_s_Instance = this;
--- Zombie.cpp
+++ Zombie.cpp
@@ -6,11 +6,16 @@
 using namespace std;
 
 bool Zombie::hit() {
-
+	m_Health--;
+	if (m_Health <= 0) {
+		m_Alive = false;
+		m_Sprite.setTexture(TextureHolder::GetTexture("graphics/blood.png"));
+		return true;
+	}
 }
 
 bool Zombie::isAlive() {
-
+	return m_Alive;
 }
 
 void Zombie::spawn(float startX, float startY, int type, int seed) {
@@ -43,13 +48,31 @@ void Zombie::spawn(float startX, float startY, int type, int seed) {
 }
 
 FloatRect Zombie::getPosition() {
-
+	return m_Sprite.getLocalBounds();
 }
 
 Sprite Zombie::getSprite() {
-
+	return m_Sprite;
 }
 
 void Zombie::update(float elapsedTime, Vector2f playerLocation) {
+	float playerX = playerLocation.x;
+	float playerY = playerLocation.y;
 
+	if (playerX > m_position.x) {
+		m_position.x = m_Speed * elapsedTime;
+	}
+	if (playerY > m_position.y) {
+		m_position.y = m_Speed * elapsedTime;
+	}
+	if (playerX < m_position.x) {
+		m_position.x = m_Speed * elapsedTime;
+	}
+	if (playerY < m_position.y) {
+		m_position.y = m_Speed * elapsedTime;
+	}
+
+	m_Sprite.setPosition(m_position);
+	float angle = (atan2(playerY - m_position.y, playerX - m_position.x) * 180) / 3.141;
+	m_Sprite.setRotation(angle);
 }
